939a3585a91f084602b17c17761aba640d39d08a
jpschmidt97 <mail@jp-schmidt.de>
2025-04-12 18:13:12 +0200

    update

--- Code/boot.py
+++ Code/boot.py
@@ -1,3 +1,21 @@
 #boot.py - wird als aller erstes beim Start des Pico W ausgeführt
 
+import network
+import time
 
+def connect_wifi():
+    print('Verbindung wird hergestellt...')
+    ssid = "Vodafone-B3C7"
+    password = "XVqMLiVMfVe763RP"
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('Verbindung wird weiterhin hergestellt...')
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            time.sleep(1)
+    print('Verbunden mit:', ssid)
+    print('IP-Adresse:', wlan.ifconfig()[0])
+
+# Aufruf der Funktion, um WLAN zu verbinden
+connect_wifi()
--- Code/main.py
+++ Code/main.py
@@ -1,3 +1,30 @@
 #main.py - wird ausgeführt, nachdem die boot.py ausgeführt wurde
 
+#Unterprogramme werden im Ordner /lib/ abgelegt und von dort aufgerufen
+#Aufrufen von Programmen aus/lib/ mit dem Befehl: import lib.ProgrammName
+    #in den Unterprogrammen in /lib/ wird mittels Threading gearbeitet
+    #dadurch läuft die main.py parallel weiter zu den Unterprogrammen
+    #Beispielprogramm:
+#        import machine
+#        import time
+#        import _thread  # Modul für Threads
+#
+#        # Funktion zum Blinken der LED
+#        def blink_led(pin_number):
+#            led = machine.Pin(pin_number, machine.Pin.OUT)
+#            while True:
+#                led.toggle()
+#                time.sleep(1)
+#
+#       # Starte den Blink-Thread
+#        _thread.start_new_thread(blink_led, (15,))
+#        #die Kommas sind wichtig, da diese zum einen die 15 als Tupel und blink_LED als Funktion an den Thread übergeben
+      
+      
+      
 
+print("main.py wurde gestartet.")
+
+import lib.RGB_LED_test
+
+print("Threading funktioniert.")
